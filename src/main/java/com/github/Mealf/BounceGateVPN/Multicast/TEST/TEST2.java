b53b43eeaeee54e3e6706b31d7c71a9798b444aa
Mealf <q100056@gmail.com>
2020-06-06 13:40:38 +0800

    Add TEST2 and Delete App.java

---
+++ src/main/java/com/github/Mealf/BounceGateVPN/Multicast/TEST/TEST2.java
@@ -0,0 +1,29 @@
+package com.github.Mealf.BounceGateVPN.Multicast.TEST;
+
+import com.github.Mealf.BounceGateVPN.Multicast.IGMPAnalysis;
+import com.github.smallru8.driver.tuntap.TapDevice;
+import java.util.Scanner;
+
+public class TEST2 {
+	public static TapDevice td;
+	public static void main(String[] args) {
+		
+		td = new TapDevice();
+    	td.startEthernetDev();
+    	td.tap.tuntap_set_ip("192.168.87.2", 24);
+    	IGMPAnalysis analysis = new IGMPAnalysis();
+    	Scanner scanner = new Scanner(System.in);
+    	scanner.hasNext();
+    	byte[] packet = analysis.generalQuery(-536870911, 2);
+    	td.write(packet);
+    	StringBuilder sb = new StringBuilder();
+	    sb.append("[ ");
+	    for (byte b : packet) {
+	        sb.append(String.format("%02X ", b));
+	    }
+	    sb.append("]");
+	    System.out.println(sb);
+    	while(true);
+	}
+
+}
